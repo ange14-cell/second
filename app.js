2268ba97c1135f42d232f3a12cb7fa1e59b92992
ange14-cell <kammitankeu14@gmail.com>
2022-11-28 16:51:02 +0100

    ajout d'un evenement

---
+++ app.js
@@ -0,0 +1,12 @@
+const btn = document.querySelector('.bouton');
+const titre = document.querySelector('h2');
+
+btn.addEventListener('click', presentation);
+
+
+
+function presentation(){
+    titre.classList.add('taille-button');
+    
+
+}
